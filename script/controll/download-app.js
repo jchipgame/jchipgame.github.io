fcf2ae681969fc2e618580f051604f7fe4774201
Jiang Chang <Jiang.Chang@cra-arc.gc.ca>
2023-04-19 14:58:24 -0400

    continue...

--- script/controll/download-app.js
+++ script/controll/download-app.js
@@ -1,4 +1,4 @@
 app.controller('download-app-controll', function($scope, $http) {
-	  $http.get("data/download-app.json?i=3")
-	  .then(function (response) {$scope.apps = response.data.apps;});
-	});
+	$http.get("data/download-app.json?i=3")
+		.then(function (response) {$scope.apps = response.data.apps;});
+});
