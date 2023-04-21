5e574e43720835844858fdb1ecf9ba55dc8e5fea
Jiang Chang <Jiang.Chang@cra-arc.gc.ca>
2023-04-21 16:23:01 -0400

    continue

--- script/service/site-game.js
+++ script/service/site-game.js
@@ -1,9 +1,9 @@
 app.service('gameService', function ($q, $http) {
     console.log("site-game-service loading ... ...");
 
-    this.gameIndex = 0;
-    this.stageIndex = -1;
-    this.gameStages = null;
+//    this.gameIndex = 0;
+//    this.stageIndex = -1;
+//    this.gameStages = null;
     
 //    $http.get("data/site-game.json?i=0", { cache: false })
 //    	.then(function (response) {
@@ -16,20 +16,15 @@ app.service('gameService', function ($q, $http) {
     this.loadGame = function (gameIndex) {
         console.log("laod start loading");
 
-        this.gameIndex = gameIndex;
+//        this.gameIndex = gameIndex;
         console.log("load start loading -- " +  this.gameIndex);
 
-        if(!this.data) {
-        	   var deferred = $q.defer();
-            console.log("no data, start http load index:" + this.gameIndex);
-            $http.get("data/site-game.json?i=0", { cache: false }).then(function (response) {
-            	this.gameStages = response.data.stages;
-                console.log("XXXX  :" + response.data);
-                console.log("UUUU  :" + response.data.stages);
-                deferred.resolve(response.data.stages);
-            });
-        //    console.log("length:" + data);
-          //  console.log("length:" + data.stages);
+        var deferred = $q.defer();
+        console.log("no data, start http load index:" + this.gameIndex);
+        $http.get("data/site-game.json?i=0", { cache: false }).then(function (response) {
+  //     	this.gameStages = response.data.stages;
+            deferred.resolve(response.data.stages);
+        });
         return deferred.promise;
                
     //   return data;
@@ -39,10 +34,6 @@ app.service('gameService', function ($q, $http) {
 //            console.log("length:" + this.data.stages);
 //                   // console.log("data: " + Object.prototype.toStrinRg.call( this.data[this.gameIndex]));
 //            return this.data[this.gameIndex];
-        } else {
-            console.log("existing data, nothing to do");
-        }
-        return null;
 	}
 
     this.getStage = function (gameIndex, stageIndex) {
