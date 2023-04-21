6716ebbc60abda92a676c6675c5fc9dec3371e1a
Jiang Chang <Jiang.Chang@cra-arc.gc.ca>
2023-04-21 16:14:54 -0400

    game json is working

--- script/controll/download-app.js
+++ script/controll/download-app.js
@@ -1,8 +1,8 @@
 app.controller('download-app-controll', function($scope, $http) {
     console.log("download-app-controll loading");
 
-	
-	
 	$http.get("data/download-app.json?i=0")
-		.then(function (response) {$scope.apps = response.data.apps;});
+		.then(function (response) {
+			$scope.apps = response.data.apps;
+		});
 });
--- script/service/site-game.js
+++ script/service/site-game.js
@@ -1,8 +1,17 @@
-app.service('gameService', function ($http) {
-    console.log("gameService loading");
+app.service('gameService', function ($q, $http) {
+    console.log("site-game-service loading ... ...");
 
     this.gameIndex = 0;
     this.stageIndex = -1;
+    this.gameStages = null;
+    
+//    $http.get("data/site-game.json?i=0", { cache: false })
+//    	.then(function (response) {
+//    		this.gameStages = response.data.stages;
+//    		console.log("XXX:" + response.data);
+//    		console.log("XXX:" + response.data.stages);
+//    	});
+    
 
     this.loadGame = function (gameIndex) {
         console.log("laod start loading");
@@ -11,11 +20,25 @@ app.service('gameService', function ($http) {
         console.log("load start loading -- " +  this.gameIndex);
 
         if(!this.data) {
+        	   var deferred = $q.defer();
             console.log("no data, start http load index:" + this.gameIndex);
-            this.data = $http.get("data/site-game.json?i=0").data;
-            console.log("length:" + this.data);
-                   // console.log("data: " + Object.prototype.toStrinRg.call( this.data[this.gameIndex]));
-            return this.data[this.gameIndex];
+            $http.get("data/site-game.json?i=0", { cache: false }).then(function (response) {
+            	this.gameStages = response.data.stages;
+                console.log("XXXX  :" + response.data);
+                console.log("UUUU  :" + response.data.stages);
+                deferred.resolve(response.data.stages);
+            });
+        //    console.log("length:" + data);
+          //  console.log("length:" + data.stages);
+        return deferred.promise;
+               
+    //   return data;
+            
+//            this.data = $http.get("data/site-game.json?i=0");
+//            console.log("length:" + this.data);
+//            console.log("length:" + this.data.stages);
+//                   // console.log("data: " + Object.prototype.toStrinRg.call( this.data[this.gameIndex]));
+//            return this.data[this.gameIndex];
         } else {
             console.log("existing data, nothing to do");
         }
@@ -24,14 +47,14 @@ app.service('gameService', function ($http) {
 
     this.getStage = function (gameIndex, stageIndex) {
         this.stageIndex = stageIndex;
-
         var stages = this.loadGame(gameIndex);
     	return stages[this.stageIndex];
 	}
 
     this.getStageCount = function (gameIndex) {
         var stages = this.loadGame(gameIndex);
-    	return stages.length;
+        console.log("stages.length="+stages.length);
+        return stages.length;
 	}
 
 });
