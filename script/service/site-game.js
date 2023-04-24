0e8375c26565d1da5c3924e4a32df791dc1e9b31
Jiang Chang <Jiang.Chang@cra-arc.gc.ca>
2023-04-24 11:41:23 -0400

    fix a bug

--- script/service/site-game.js
+++ script/service/site-game.js
@@ -2,11 +2,11 @@ app.service('gameService', function ($q, $http) {
     console.log("site-game-service loading ... ...");
 
     this.loadGame = function (gameIndex) {
-        console.log("load start loading -- " +  this.gameIndex);
+        console.log("loadGame start -- " +  gameIndex);
         var deferred = $q.defer();
-        console.log("no data, start http load index:" + this.gameIndex);
+        console.log("no data, start http load index:" + gameIndex);
         $http.get("data/site-game.json?i=2", { cache: false }).then(function (response) {
-            // console.log("service json data -- " +  response.data.stages);
+            console.log("service site-game.json data = " +  response.data.stages);
             deferred.resolve(response.data.stages[gameIndex]);
         });
         return deferred.promise;
