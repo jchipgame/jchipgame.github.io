752da64c6db0f4532e45cff795fdac58176fd29d
Jiang Chang <Jiang.Chang@cra-arc.gc.ca>
2023-04-24 09:18:56 -0400

    service is working

--- script/controll/site-game.js
+++ script/controll/site-game.js
@@ -12,25 +12,38 @@ app.controller('site-game-controll', function($scope, gameService) {
     ];
 
 	
-    $scope.selectGame = function(index) {
-        $scope.gameIndex = index;
+    $scope.selectGame = function(gameIndex) {
+        $scope.gameIndex = gameIndex;
         $scope.stageIndex = -1;
         $scope.stageMatrix = null;
-        console.log("game gameIndex:" +  $scope.gameIndex );
-        $scope.stageCount = gameService.getStageCount(index, -1);
-        console.log("game stageCount:" +  $scope.stageCount );
+        gameService.loadGame($scope.gameIndex).then(function (response) {
+            console.log("UU game gameIndex:" +  $scope.gameIndex);
+            var stages = response;
+            console.log("UU payload data:" +  stages);
+            $scope.stageCount = stages.length;
+            console.log("UU $scope.stageCount:" + $scope.stageCount);
+        });
     };
     
-    $scope.selectStage = function(index) {
-        $scope.stageIndex = index;
-        $scope.stageMatrix = gameService.getStage($scope.stageIndex, $scope.stageIndex);
-        console.log("game stageMatrix:" +  $scope.stageMatrix );
-        
+    $scope.selectStage = function(gameIndex, stageIndex) {
+        $scope.stageIndex = stageIndex;
+        gameService.loadGame($scope.gameIndex).then(function (response) {
+            console.log("UU game gameIndex:" +  $scope.gameIndex);
+            var stages = response;
+            console.log("UU payload data:" +  stages);
+            console.log("UU $scope.stageIndex:" +  $scope.stageIndex);
+            $scope.stageCount = stages.length;
+            $scope.stageMatrix = stages[$scope.stageIndex];
+            console.log("UUX $scope.stageCount:" + $scope.stageCount);
+            console.log("UUX $scope.stageMatrix:" + $scope.stageMatrix);
+            
+            $scope.loadGame($scope.stageMatrix);
+        });
     };
     
-    $scope.selectGame(0);
-    $scope.selectStage(0);
+    $scope.loadGame = function(matrix) {
+    	_load(matrix, _element('container'), _element('titler'));
+    }
     
-	_load($scope.stageMatrix, _element('container'), _element('titler'));
-
+    $scope.selectStage(0, 0);
 });
--- script/service/site-game.js
+++ script/service/site-game.js
@@ -1,51 +1,18 @@
 app.service('gameService', function ($q, $http) {
     console.log("site-game-service loading ... ...");
 
-//    this.gameIndex = 0;
-//    this.stageIndex = -1;
-//    this.gameStages = null;
-    
-//    $http.get("data/site-game.json?i=0", { cache: false })
-//    	.then(function (response) {
-//    		this.gameStages = response.data.stages;
-//    		console.log("XXX:" + response.data);
-//    		console.log("XXX:" + response.data.stages);
-//    	});
-    
-
     this.loadGame = function (gameIndex) {
-        console.log("laod start loading");
-
-//        this.gameIndex = gameIndex;
         console.log("load start loading -- " +  this.gameIndex);
 
         var deferred = $q.defer();
         console.log("no data, start http load index:" + this.gameIndex);
-        $http.get("data/site-game.json?i=0", { cache: false }).then(function (response) {
+        $http.get("data/site-game.json?i=2", { cache: false }).then(function (response) {
   //     	this.gameStages = response.data.stages;
-            deferred.resolve(response.data.stages);
+            console.log("service json data -- " +  response.data.stages);
+        	
+            deferred.resolve(response.data.stages[gameIndex]);
         });
         return deferred.promise;
-               
-    //   return data;
-            
-//            this.data = $http.get("data/site-game.json?i=0");
-//            console.log("length:" + this.data);
-//            console.log("length:" + this.data.stages);
-//                   // console.log("data: " + Object.prototype.toStrinRg.call( this.data[this.gameIndex]));
-//            return this.data[this.gameIndex];
-	}
-
-    this.getStage = function (gameIndex, stageIndex) {
-        this.stageIndex = stageIndex;
-        var stages = this.loadGame(gameIndex);
-    	return stages[this.stageIndex];
-	}
-
-    this.getStageCount = function (gameIndex) {
-        var stages = this.loadGame(gameIndex);
-        console.log("stages.length="+stages.length);
-        return stages.length;
 	}
 
 });
