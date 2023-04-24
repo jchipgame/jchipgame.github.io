8b481b2e8680131e4bc560dfb58d14fe3557b281
Jiang Chang <Jiang.Chang@cra-arc.gc.ca>
2023-04-24 11:46:23 -0400

    bug fix for game select.

--- script/service/site-game.js
+++ script/service/site-game.js
@@ -6,7 +6,7 @@ app.service('gameService', function ($q, $http) {
         var deferred = $q.defer();
         console.log("no data, start http load index:" + gameIndex);
         $http.get("data/site-game.json?i=2", { cache: false }).then(function (response) {
-            console.log("service site-game.json data = " +  response.data.stages);
+            // console.log("service site-game.json data = " +  response.data.stages);
             deferred.resolve(response.data.stages[gameIndex]);
         });
         return deferred.promise;
