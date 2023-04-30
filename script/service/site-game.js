2e182baf0d9caaf4fc4798cef0e37cb622db9c55
jiangchang <jiangchang@gmail.com>
2023-04-30 10:42:53 -0400

    good work

--- script/service/site-game.js
+++ script/service/site-game.js
@@ -1,14 +1,18 @@
 app.service('gameService', function ($q, $http) {
     console.log("site-game-service loading ... ...");
-
+	this.stages = null;
     this.loadGame = function (gameIndex) {
         console.log("loadGame start -- " +  gameIndex);
         var deferred = $q.defer();
         console.log("start http load index:" + gameIndex);
-        $http.get("data/site-game.json?i=2", { cache: false }).then(function (response) {
-            // console.log("service site-game.json data = " +  response.data.stages);
-            deferred.resolve(response.data.stages[gameIndex]);
+        if(this.stages == null) {
+        $http.get("data/site-game.json", { cache: false }).then(function (response) {
+            this.stages = response.data.stages
+            deferred.resolve(this.stages[gameIndex]);
         });
+        } else {
+        	deferred.resolve(this.stages[gameIndex]);
+        }
         return deferred.promise;
 	}
 
